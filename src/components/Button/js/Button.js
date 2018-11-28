a9cda175c81edc0aa6546165c495c47b44ad0d12
Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
2018-11-28 08:25:08 +0200

    added outline buttons

--- src/components/Button/js/Button.js
+++ src/components/Button/js/Button.js
@@ -1,15 +1,121 @@
-import React, { Component } from 'react';
-import '../sass/button.scss';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 
+import '../sass/button.scss';
+/**
+ * Tyk button component.
+ */
 export default class Button extends Component {
+  static propTypes = {
+    /**
+    * Button id.
+    */
+    buttonId: PropTypes.string,
+    /**
+    * Content of a button. Can be a DOM element or a React component
+    */
+    children: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.string
+    ]),
+    /**
+    * Display type: inline/block. Default is inline
+    */
+    display: PropTypes.string,
+    /**
+    * Sets the button disabled state
+    */
+    disabled: PropTypes.bool,
+    /**
+    * Having this property set the button will become a link
+    */
+    href: PropTypes.string,
+    iconPosition: PropTypes.string, //left or right
+    iconType: PropTypes.string,
+    /**
+    * Callback function, called when the button si clicked
+    */
+    onClick: PropTypes.func,
+    /**
+    * Describes the appearence of the button (ex: primary, danger, primary-outline etc.)
+    */
+    theme: PropTypes.string,
+    /**
+    * Type of the button (ex: button, submit etc.)
+    */
+    type: PropTypes.string,
+    /**
+    * Describes the size of the button (sm, md, lg). Default one is md
+    */
+    size: PropTypes.string
+  }
+
+  static defaultProps = {
+    theme: ''
+  }
+
+  getCssClasses() {
+    const {
+      iconType,
+      iconPosition,
+      className,
+      size,
+      disabled,
+      display,
+      theme
+    } = this.props;
+
+    let cssClasses = ['tyk-button'].concat(className ? className.split(' ') : []);
+    let sizeClass = (size) ? `tyk-button--${size}` : 'tyk-button--md';
+    let disabledClass = (disabled) ? 'disabled' : '';
+    let displayClass = display || '';
+    let themeClass = theme ? `tyk-button--${theme}` : '';
+
+    cssClasses.push(themeClass);
+    cssClasses.push(sizeClass);
+    cssClasses.push(disabledClass);
+    cssClasses.push(displayClass);
+
+    if(iconType) {
+      cssClasses.push('icon-' + (iconPosition || 'left'));
+    }
+
+    return cssClasses.join(' ');
+  }
+
   render() {
+    const {
+      id,
+      disabled,
+      href,
+      onClick,
+      type } = this.props;
+
     return (
-      <button
-        className="tyk-button tyk-button--default"
-        type="button"
-      >
-        Button
-      </button>
+      <Fragment>
+      {
+        href
+          ? <a
+              id={ id }
+              className={ this.getCssClasses() }
+              disabled={ disabled }
+              onClick={ onClick }
+              href={ href }
+              type={ type }
+            >
+              { this.props.children }
+            </a>
+          : <button
+              id={ id }
+              className={ this.getCssClasses() }
+              disabled={ disabled }
+              onClick={ onClick }
+              type={ type }
+            >
+              { this.props.children && <span>{ this.props.children }</span> }
+            </button>
+      }
+      </Fragment>
     );
   }
 }
---
+++ src/components/Button/js/Readme.md
@@ -0,0 +1,19 @@
+**Button Themes**
+
+```js
+<Button theme="primary">Push Me</Button> &ensp;
+<Button theme="success">Push Me</Button> &ensp;
+<Button theme="info">Push Me</Button> &ensp;
+<Button theme="warning">Push Me</Button> &ensp;
+<Button theme="danger">Push Me</Button> &ensp;
+<Button theme="default">Push Me</Button> &ensp;
+<Button>Push Me</Button>
+```
+```js
+<Button theme="primary-outline">Push Me</Button> &ensp;
+<Button theme="success-outline">Push Me</Button> &ensp;
+<Button theme="info-outline">Push Me</Button> &ensp;
+<Button theme="warning-outline">Push Me</Button> &ensp;
+<Button theme="danger-outline">Push Me</Button> &ensp;
+<Button theme="default-outline">Push Me</Button> &ensp;
+```
