0839192387ac2b4e2d1618ae818d22e140e32e51
Vlad Ifrim <ifrimvlad@gmail.com>
2019-12-22 22:19:52 +0200

    export combobox 2; add and export formik combobox 2 (#46)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ export { default as Collapsible } from './components/Collapsible';
 export { default as Chart } from './components/Chart';
 export { default as Column } from './components/Column';
 export { Combobox, FieldCombobox, FormikCombobox } from './components/Combobox';
+export { Combobox2, FormikCombobox2 } from './components/Combobox2';
 export { DatePicker, FieldDatePicker, FormikDatePicker } from './components/DatePicker';
 export { Dropdown, FieldDropdown } from './components/Dropdown';
 export { EditableList, FieldEditableList, FormikEditableList } from './components/EditableList';
