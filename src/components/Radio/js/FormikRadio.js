854c1c239d89cb95d263ffd245164b56267b9aab
Vlad Ifrim <ifrimvlad@gmail.com>
2020-01-27 12:00:32 +0200

    fix bug with formik radio component support (#65)

--- src/components/Radio/js/FormikRadio.js
+++ src/components/Radio/js/FormikRadio.js
@@ -2,5 +2,8 @@ import wrapper from '../../../common/js/utils/formik';
 import Radio from './Radio';
 
 export default wrapper(Radio, {
-  getOnChangeProps: (value, field, form, properties) => ({ checked: value === properties.value }),
+  getOnChangeProps: (value, field, form, properties) => ({
+    checked: value === properties.value,
+    value: properties.value,
+  }),
 });
