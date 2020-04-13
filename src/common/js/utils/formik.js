31aeaddd5d6e682bfd79d259a8d151194983a698
Vlad Ifrim <ifrimvlad@gmail.com>
2020-04-13 15:23:58 +0300

    formik wrapper bug fix: update component value when field value changes (#116)

--- src/common/js/utils/formik.js
+++ src/common/js/utils/formik.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function getValueFromPath(obj, path) {
   // turn 'aaa.qqq[2].bbb[5][3].mmm' into ['aaa', 'qqq', '2', 'bbb', '5', '3', 'mmm']
@@ -14,7 +14,7 @@ function debounce(f, limit) {
   };
 }
 
-const triggerFieldOnChange = debounce((field, e) => field.onChange(e), 200);
+const executeDebounced = debounce(fn => fn(), 200);
 
 /* eslint-disable react/prop-types */
 const wrapper = (Component, options) => ({ field, form, ...properties }) => {
@@ -41,19 +41,23 @@ const wrapper = (Component, options) => ({ field, form, ...properties }) => {
     const onChangeProps = opts.getOnChangeProps(value, field, form, properties);
 
     const newValue = onChangeProps.value || properties.value || value;
-    if (typeof properties.onChange === 'function') {
-      properties.onChange(newValue);
-    }
-    triggerFieldOnChange(
-      field,
-      { target: { name: field.name, value: newValue, ...onChangeProps } },
-    );
+    executeDebounced(() => {
+      if (typeof properties.onChange === 'function') {
+        properties.onChange(newValue);
+      }
+      field.onChange({ target: { name: field.name, value: newValue, ...onChangeProps } });
+    });
     setMyValue(newValue);
   };
 
   const formError = (getValueFromPath(form.touched, field.name) || Boolean(form.submitCount))
     && getValueFromPath(form.errors, field.name);
   const error = typeof formError === 'string' ? formError : '';
+
+  useEffect(() => {
+    setMyValue(field.value);
+  }, [field.value]);
+
   return (
     <Component
       {...field}
