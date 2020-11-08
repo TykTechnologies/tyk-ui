ac500e2caeed914a74ead1a0adad12d54bf78b7c
Jay <jaydeshmukh8551@gmail.com>
2020-11-08 20:15:25 +0530

    fix duplicate keys bug

--- src/components/StringBuilder/js/tokenized-string.js
+++ src/components/StringBuilder/js/tokenized-string.js
@@ -2,24 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const TokenizedString = ({ tokens, options }) => {
-  const allTokens = tokens && tokens.map((token) => {
-    if (options) {
-      const matchedOption = options.find(option => option.id === token);
-      if (matchedOption) {
-        return (
-          <span key={token} className={`${matchedOption.className || 'default-option-name'}`}>
-            {matchedOption.id}
-          </span>
-        );
+  const hasDuplicates = +new Set(tokens).size !== tokens.length;
+  console.log();
+  const allTokens = tokens
+    && tokens.map((token) => {
+      if (options) {
+        const matchedOption = options.find(option => option.id === token);
+        if (matchedOption) {
+          return (
+            <span
+              key={`${token}${hasDuplicates && Math.random()}`}
+              className={`${matchedOption.className || 'default-option-name'}`}
+            >
+              {matchedOption.id}
+            </span>
+          );
+        }
       }
-    }
-    return <span key={token}>{token}</span>;
-  });
+      return (
+        <span key={`${token}${hasDuplicates && Math.random()}`}>{token}</span>
+      );
+    });
   return <div className="string-builder__styled">{allTokens}</div>;
 };
 
 TokenizedString.propTypes = {
   tokens: PropTypes.arrayOf(PropTypes.string),
   options: PropTypes.arrayOf(PropTypes.instanceOf(Object)),
-
 };
