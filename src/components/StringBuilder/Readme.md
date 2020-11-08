411265b5ba6372986aea4d41a7636a603124ffeb
Jay <jaydeshmukh8551@gmail.com>
2020-11-08 22:39:53 +0530

    invalid token detection v1

--- src/components/StringBuilder/Readme.md
+++ src/components/StringBuilder/Readme.md
@@ -8,8 +8,8 @@
   // value="http://sample.com/{{.arguments.userId}}?id={{.arguments.userId}}&name={{.object.name}}/alphabrovodeltacharlieanddelta-{{.object.location}}/alfhadsflhasdiufgysudagfiasdgfyiduasgfisadfhoisudhfo/{{.object.name}}/location}}/alfhadsflhasdiufgysudagfiasdgfyiduasgfisadfhoisudhfo/{{.object.name}}/location}}/alfhadsflhasdiufgysudagfiasdgfyiduasgfisadfhoisudhfo/{{.object.name}}"
   // value="http://sample.com/{{.arguments.userId}}?id={{.arguments.userId}}"
   // value="http://sample.com/{{.arguments.userId}}?id={{.arguments.userId}}&name={{.object.name}}/alphabrovodeltacharlieanddelta-{{.object.location}}/alfhadsflhasdiufgysudagfiasdgfyiduasgfisadfhoisudhfo/{{.object.name}}/alphdsbfkjdsbfkjdsnfjklfn;asdflajsdfkjhsdavfksdhvajhsdhdsfsdfsfsdflmknbkahjdsfgks/{{.object.pin}}/{{.object.pin}}/{{.object.name}}"
-  // value="http://helloWorld.com/{{.object.name}}/id={{.object.pin}}"
-  value="http://alpha.com/getLocation/{{.object.name}}/{{.object.location}}/{{.object.location}}/hello/{{.object.name}}"
+  value="http://helloWorld.com/{{.object.name}}/snekjbfkejw/{id}"
+  // value="http://alpha.com/getLocation/{{.object.name}}/{{.object.location}}/{{.object.location}}/hello/{{.object.name}}"
   placeholder="Please enter your url"
   onChange={(v) => console.log("Value", v)}
   theme="default rounded-corners"
--- src/components/StringBuilder/js/tokenized-string.js
+++ src/components/StringBuilder/js/tokenized-string.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tooltip from '../../Tooltip';
+// import Button from '../../Button';
 
 export const TokenizedString = ({ tokens, options }) => {
   const hasDuplicates = +new Set(tokens).size !== tokens.length;
-  console.log();
+
   const allTokens = tokens
     && tokens.map((token) => {
       if (options) {
+        // if token matches option
         const matchedOption = options.find(option => option.id === token);
         if (matchedOption) {
           return (
@@ -18,6 +21,24 @@ export const TokenizedString = ({ tokens, options }) => {
             </span>
           );
         }
+      }
+      // if invalid token
+      const invalidToken = token.match(/({.*?})/g);
+      if (invalidToken) {
+        const splitTokens = token.split(invalidToken);
+        console.log({ token, splitTokens, invalidToken });
+        return (
+          <span key={`${token}${hasDuplicates && Math.random()}`}>
+            <span>{splitTokens[0]}</span>
+            <Tooltip
+              render="invalid token detected"
+              position="top"
+            >
+              <span className="invalid_token">{invalidToken}</span>
+            </Tooltip>
+            <span>{splitTokens[1]}</span>
+          </span>
+        );
       }
       return (
         <span key={`${token}${hasDuplicates && Math.random()}`}>{token}</span>
