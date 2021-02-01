1fbcc613f3757c3c6e4f38b87758be9cc69d19fc
lghiur <laurentiu.ghiur@gmail.com>
2021-02-01 13:44:32 +0200

    removed dummy initial value

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -8,7 +8,7 @@ import AccordionContext from './js/AccordionContext';
 
 const Accordion = (props) => {
   const {
-    className = 'aaa',
+    className,
     children,
     usearrowastrigger,
     arrow = { position: 'right', expandToContent: false },
