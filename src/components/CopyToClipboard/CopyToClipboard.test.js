1cb76c090501b30770684713190ae4734729ce91
lghiur <laurentiu.ghiur@gmail.com>
2023-06-01 15:59:36 +0300

    Revert "adds tests for the copy to clipboard component"
    This reverts commit 42526ab6e9be52a3958d5c91d6b5d2ef42eabaea.

--- src/components/CopyToClipboard/CopyToClipboard.test.js
+++ src/components/CopyToClipboard/CopyToClipboard.test.js
@@ -1,70 +1,7 @@
-import React from 'react';
+import CoopyToClipboard from './index';
 
-import CopyToClipboard from './index';
-
-describe('CopyToClipboard', () => {
-  const content = 'The Content';
-  const textToCopy = 'this is copied';
-
-  it('renders the component with the content', () => {
-    cy
-      .mount(
-        <CopyToClipboard element="span" copy={textToCopy}>
-          <h1>{content}</h1>
-        </CopyToClipboard>,
-      )
-      .get('h1')
-      .contains(content)
-      .should('exist');
-  });
-
-  it('accepts a prop called "copy" that represents the text copied to the clipboard', () => {
-    cy
-      .mount(
-        <CopyToClipboard element="span" copy={textToCopy}>
-          <h1 tabIndex="0">{content}</h1>
-        </CopyToClipboard>,
-      )
-      .get('h1')
-      .focus()
-      .realClick()
-      .then(() => {
-        cy.window()
-          .its('navigator.clipboard')
-          .invoke('readText')
-          .should('equal', textToCopy);
-      });
-  });
-
-  it('can use the "display" prop to render content', () => {
-    cy
-      .mount(<CopyToClipboard element="span" display={content} copy={textToCopy} />)
-      .get('span')
-      .contains(content)
-      .should('exist');
-  });
-
-  it('can specify a different element to wrap the content', () => {
-    cy
-      .mount(<CopyToClipboard element="p" display={content} copy={textToCopy} />)
-      .get('p')
-      .contains(content)
-      .should('exist');
-  });
-
-  it('calls the onCopy callback when copying', () => {
-    const onCopy = cy.stub();
-    cy
-      .mount(
-        <CopyToClipboard element="span" copy={textToCopy} onCopy={onCopy}>
-          <h1 tabIndex="0">{content}</h1>
-        </CopyToClipboard>,
-      )
-      .get('h1')
-      .focus()
-      .realClick()
-      .then(() => {
-        expect(onCopy).to.be.called;
-      });
+describe('CoopyToClipboard', () => {
+  it('TODO', () => {
+    expect(true).to.equal(true);
   });
 });
--- src/components/CopyToClipboard/index.js
+++ src/components/CopyToClipboard/index.js
@@ -1,4 +1,4 @@
-import React, { createRef, useCallback } from 'react';
+import React, { Fragment, createRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import toast from '../Toast';
@@ -8,9 +8,10 @@ import toast from '../Toast';
  * - Can be used normally with a text or a custom components (Eg. Button, Icon, etc)
  */
 
-function CopyToClipboard({
-  display, copy, children, onCopy, element: Element, ...rest
-}) {
+const CopyToClipboard = (props) => {
+  const {
+    display, copy, children, onCopy,
+  } = props;
   const txtRef = createRef();
   const handleClick = useCallback(() => {
     if (onCopy) {
@@ -20,26 +21,26 @@ function CopyToClipboard({
     if (!window.navigator.clipboard) {
       txtRef.current.select();
       document.execCommand('copy');
-      toast.success('copied');
+      toast.notify('copied', { theme: 'success' });
       return;
     }
     window.navigator.clipboard.writeText(copy);
-    toast.success('copied');
+    toast.notify('copied', { theme: 'success' });
   }, [onCopy, txtRef]);
 
   return (
-    <>
-      <Element onClick={handleClick} onKeyUp={handleClick} {...rest}>
+    <Fragment>
+      <props.element onClick={handleClick} onKeyUp={handleClick} {...props}>
         {children || display}
-      </Element>
+      </props.element>
       {!window.navigator.clipboard ? (
         <textarea ref={txtRef} className="tyk-copy-to-clipboard" name="copy" value={copy}>
           {copy}
         </textarea>
       ) : null}
-    </>
+    </Fragment>
   );
-}
+};
 
 CopyToClipboard.propTypes = {
   /** Callback function executed after text is copied */
@@ -48,13 +49,6 @@ CopyToClipboard.propTypes = {
   copy: PropTypes.string.isRequired,
   /** Text to be displayed */
   display: PropTypes.string,
-  /** Element to wrap the content with */
-  element: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.string,
-    PropTypes.object,
-    PropTypes.node,
-  ]),
   /** Children of custom element if you are using any custom element */
   children: PropTypes.oneOfType([
     PropTypes.element,
