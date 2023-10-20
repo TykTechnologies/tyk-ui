4acdcb2e7724f8c222f9884ac0f6927148ff2cb6
Jay <jaydeshmukh8551@gmail.com>
2023-10-20 12:03:14 +0530

    add cypress test for list component

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,7 +1,28 @@
+import React from 'react';
 import List from './index';
 
 describe('List', () => {
-  it('TODO', () => {
-    expect(true).to.equal(true);
+  it('test list is rendered with items in primary theme', () => {
+    cy.mount(
+      <List
+        theme="primary-light"
+        label="LABEL"
+      >
+        <List.Item>Alpha</List.Item>
+        <List.Item>Bravo</List.Item>
+        <List.Item>Charlie</List.Item>
+        <List.Item>Delta</List.Item>
+      </List>
+    )
+    
+    cy.get('.tyk-list__wrapper')
+      .get('ul')
+      .should('have.class', 'tyk-list')
+
+    cy.contains("LABEL").should('exist');
+    cy.contains("Alpha").should('exist');
+    cy.contains("Bravo").should('exist');
+    cy.contains("Charlie").should('exist');
+    cy.contains("Delta").should('exist');
   });
 });
