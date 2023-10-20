0239331a87177ae41a62c300715208f1a0f2cbd7
Jay Deshmukh <35005482+jay-deshmukh@users.noreply.github.com>
2023-10-20 11:40:19 +0530

    add test for CopyToClipboard component (#304)
    * add test for CopyToClipboard component
    
    * modify test description

--- src/components/CopyToClipboard/CopyToClipboard.test.js
+++ src/components/CopyToClipboard/CopyToClipboard.test.js
@@ -1,7 +1,29 @@
-import CoopyToClipboard from './index';
+import React from 'react';
+import CopyToClipboard from './index';
+import Button from '../Button'
 
 describe('CoopyToClipboard', () => {
-  it('TODO', () => {
-    expect(true).to.equal(true);
+  it('Test component rendering and Test copy functionality', () => {
+    const textToCopy = "boooo";
+    const displayText = "BUTTON_TEST_NAME"
+    cy.mount(
+      <div>
+        <CopyToClipboard
+          copy={textToCopy}
+          display={displayText}
+          theme="primary"
+          element={Button}
+          onCopy={() => {
+            document.querySelector('.dummy').innerText = textToCopy
+          }}
+        />
+        <span className='dummy'>PLACEHOLDER</span>
+      </div>
+    );
+    cy.get('span.dummy').invoke('text').should('eq', 'PLACEHOLDER'); 
+    cy.contains(textToCopy).should('not.exist');
+    cy.contains(displayText).should('exist').click()
+    cy.get('span.dummy').invoke('text').should('eq', textToCopy); 
+    
   });
 });
