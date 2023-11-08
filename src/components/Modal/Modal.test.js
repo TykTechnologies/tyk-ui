c6f2fdcdb226c37d7df536e9abd38aaccf0ce765
Vlad Ifrim <ifrimvlad@gmail.com>
2023-11-08 23:18:07 +0200

    fixes tests for the onClose callback and adds additional test to increase coverage

--- src/components/Modal/Modal.test.js
+++ src/components/Modal/Modal.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Modal from './index';
 
-function Component({ children, ...props }) {
+function Component(props) {
   return (
     <Modal {...props}>
-      <Modal.Body>{children}</Modal.Body>
+      {/* eslint-disable-next-line react/prop-types, react/destructuring-assignment */}
+      {props.children || (
+        <>
+          <Modal.Body>Demo</Modal.Body>
+          <Modal.Footer>modal footer</Modal.Footer>
+        </>
+      )}
     </Modal>
   );
 }
@@ -28,7 +34,7 @@ describe('Modal', () => {
   };
 
   it('render the modal when opened prop exists or set to true', () => {
-    cy.mount(<Component opened>demo</Component>)
+    cy.mount(<Component opened />)
       .get(selectors.tykModal)
       .should('exist')
       .and('have.class', cssClasses.tykModal)
@@ -36,92 +42,99 @@ describe('Modal', () => {
       .and('have.class', cssClasses.themeNone);
   });
 
-  it('dont render the modal when opened is set to false ', () => {
-    cy.mount(<Component>demo</Component>)
+  it('the modal is not rendered when opened is set to false ', () => {
+    cy.mount(<Component />)
       .get(selectors.tykModal)
       .and('not.have.class', cssClasses.opened)
       .find(selectors.modalDialog)
       .should('exist')
-      .and('have.css', 'opacity', '0');
+      .and('not.be.visible');
   });
 
   it('should add custom class', () => {
-    cy.mount(<Component className="custom-class">Demo</Component>)
+    cy.mount(<Component className="custom-class" />)
       .get(selectors.tykModal)
       .should('exist')
       .and('have.class', 'custom-class');
   });
 
   it('should add theme class when theme option is present', () => {
-    cy.mount(<Component opened theme="success">Demo</Component>)
+    cy.mount(<Component opened theme="success" />)
       .get(selectors.tykModal)
       .should('exist')
       .and('have.class', cssClasses.themeSuccess);
   });
 
-  it.skip('should close when back drop is clicked', () => {
-    cy.mount(<Component opened theme="success">Demo</Component>)
+  it('should call onClose when back drop is clicked', () => {
+    const onClose = cy.stub();
+    cy.mount(<Component opened onClose={onClose} />)
       .get(selectors.backDrop)
       .should('exist')
       .and('have.class', cssClasses.opened)
       .and('have.class', cssClasses.backDrop);
 
-    cy.get(selectors.backDrop).click();
-    cy
-      .get(selectors.backDrop)
-      .should('exist')
-      .and('have.not.class', cssClasses.opened);
+    cy.get(selectors.backDrop)
+      .click({ force: true })
+      .then(() => {
+        expect(onClose).to.be.called;
+      });
   });
 
-  it.skip('should not close the backdrop on click with disableCloseCommands', () => {
-    cy.mount(<Component opened theme="success">Demo</Component>)
+  it('should not call onClose with disableCloseCommands', () => {
+    const onClose = cy.stub();
+    cy.mount(<Component opened disableCloseCommands />)
       .get(selectors.backDrop)
       .should('exist')
       .and('have.class', cssClasses.opened)
       .and('have.class', cssClasses.backDrop);
 
-    cy.get(selectors.backDrop).click();
+    cy.get(selectors.backDrop)
+      .click({ force: true })
+      .then(() => {
+        expect(onClose).not.to.be.called;
+      });
     cy
       .get(selectors.backDrop)
       .should('exist')
       .and('have.class', cssClasses.opened);
   });
 
-  it.skip('should call onclose when dialog closed', () => {
-    const onCloseSpy = cy.spy();
-
-    cy.mount(<Component opened onClose={onCloseSpy} theme="success">Demo</Component>)
-      .get(selectors.backDrop)
-      .should('exist')
-      .and('have.class', cssClasses.opened)
-      .and('have.class', cssClasses.backDrop);
-
-    cy.get(selectors.backDrop).click();
-    cy
-      .get(selectors.backDrop)
-      .should('exist')
-      .and('have.not.class', cssClasses.opened);
-
-    onCloseSpy.should('be.called');
-  });
-
   it('should not display when showBackdrop is set to false', () => {
-    cy.mount(<Component opened showBackdrop={false} theme="success">Demo</Component>)
+    cy.mount(<Component opened showBackdrop={false} theme="success" />)
       .get(selectors.backDrop)
       .should('not.exist');
   });
 
   it('should set width to md when size is set to md', () => {
-    cy.mount(<Component opened size="md" showBackdrop={false} theme="success">Demo</Component>)
+    cy.mount(<Component opened size="md" showBackdrop={false} theme="success" />)
       .get(selectors.modalDialog)
       .should('exist')
       .and('have.class', cssClasses.widthMd);
   });
 
   it('should set width to lg when size is set to lg', () => {
-    cy.mount(<Component opened size="lg" showBackdrop={false} theme="success">Demo</Component>)
+    cy.mount(<Component opened size="lg" showBackdrop={false} theme="success" />)
       .get(selectors.modalDialog)
       .should('exist')
       .and('have.class', cssClasses.widthLg);
+  });
+
+  it('console warns a message if Modal.Header or Modal.Title is used', () => {
+    cy.window()
+      .its('console')
+      .then((console) => cy.spy(console, 'warn').as('warn'));
+
+    cy.mount(
+      <Component opened theme="success">
+        <Modal.Header>
+          <Modal.Title>modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>modal body</Modal.Body>
+      </Component>,
+    )
+      .get('@warn')
+      .then((warn) => {
+        expect(warn).to.be.calledTwice;
+      });
   });
 });
