12f1233973d30756ecd7e35929fa9340d28d7cee
Laurentiu Ghiur <laurentiu.ghiur@gmail.com>
2023-11-14 10:25:57 +0200

    TT-10495 added tests for text ellipsis (#317)
    * TT-10495 added tests for text ellipsis

--- src/components/TextEllipsis/TextEllipsis.test.js
+++ src/components/TextEllipsis/TextEllipsis.test.js
@@ -1,7 +1,33 @@
+import React from 'react';
 import TextEllipsis from './index';
 
+function Component() {
+  return (
+    <div className="text-ellipsis" style={{ marginTop: '200px', textAlign: 'center' }}>
+      <TextEllipsis
+        text="alpha, bravo, charlie, delta, forgot the rest"
+        limit={10}
+      />
+    </div>
+  );
+}
+
 describe('TextEllipsis', () => {
-  it('TODO', () => {
-    expect(true).to.equal(true);
+  const classes = {
+    floatingContainer: 'floating-container',
+  };
+  it('should show just first 10 chars followed by "..."', () => {
+    cy.mount(<Component />)
+      .get('.text-ellipsis')
+      .should('have.text', 'alpha, bra...');
+  });
+  it('when hovering the shrinked texts, shows the tooltip with full text', () => {
+    cy.mount(<Component />);
+    cy.get(`.${classes.floatingContainer}`)
+      .should('not.exist')
+      .get('.text-ellipsis')
+      .trigger('mouseover')
+      .get(`.${classes.floatingContainer}`)
+      .should('exist');
   });
 });
