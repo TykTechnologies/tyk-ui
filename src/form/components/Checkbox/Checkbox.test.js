98fa74295ae9db9332836d2871525648895a2041
Jay Deshmukh <35005482+jay-deshmukh@users.noreply.github.com>
2023-11-14 13:55:21 +0530

    add test for checkbox (#318)

--- src/form/components/Checkbox/Checkbox.test.js
+++ src/form/components/Checkbox/Checkbox.test.js
@@ -1,7 +1,35 @@
+import React from "react";
 import Checkbox from './index';
 
 describe('Checkbox', () => {
-  it('TODO', () => {
-    expect(true).to.equal(true);
+
+  it('Checkbox : with error', () => {
+    const label = "This is a checkbox";
+    const error = "Please fix the error"
+    cy.mount(<Checkbox
+      label={label}
+      error={error}
+      id="testCheckbox"
+    />)
+
+    cy.contains(label).should('exist')
+    cy.contains(error).should('exist')
+    cy.get('#testCheckbox').should('not.be.checked');
+    cy.contains(label).should('exist').click();
+    cy.get('#testCheckbox').should('be.checked');
+  });
+
+  it('Checkbox : with disabled checked', () => {
+    const label = "This is a checkbox";
+    cy.mount(<Checkbox
+      label={label}
+      id="testCheckbox"
+      disabled
+    />)
+
+    cy.contains(label).should('exist')
+    cy.get('#testCheckbox').should('not.be.checked');
+    cy.contains(label).should('exist').click();
+    cy.get('#testCheckbox').should('not.be.checked');
   });
 });
