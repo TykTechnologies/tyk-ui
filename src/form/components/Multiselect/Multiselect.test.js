92b711653fd24bbe21e40ddfd3d88d20f9bab52e
Vlad Ifrim <ifrimvlad@gmail.com>
2024-01-16 14:14:19 +0200

    tests for the Multiselect component

--- src/form/components/Multiselect/Multiselect.test.js
+++ src/form/components/Multiselect/Multiselect.test.js
@@ -1,7 +1,156 @@
+import React, { useState } from 'react';
 import Multiselect from './index';
 
+const defaultItems = [
+  { id: 1, name: 'item 1' },
+  { id: 2, name: 'item 2' },
+  { id: 3, name: 'item 3' },
+  { id: 4, name: 'item 4' },
+  { id: 5, name: 'item 5' },
+  { id: 6, name: 'item 6' },
+  { id: 7, name: 'item 7' },
+  { id: 8, name: 'item 8' },
+  { id: 9, name: 'item 9' },
+];
+
+function Component(props) {
+  const [items, setItems] = useState(defaultItems);
+  // eslint-disable-next-line react/prop-types
+  const { value: initialValue, ...rest } = props;
+  const [value, setValue] = useState(initialValue ?? []);
+  return (
+    <Multiselect
+      entityName="Item"
+      entityNamePlural="Items"
+      items={items}
+      fieldsToSearchOn={['name']}
+      value={value}
+      getItemDetails={(item) => setItems((xs) => xs.map((x) => (
+        x.id === item.id
+          ? { ...x, details: { ...x } }
+          : x
+      )))}
+      itemDisplayTemplate={({ name }) => name}
+      detailsDisplayTemplate={({ id, name }) => `${name} (${id})`}
+      onChange={setValue}
+      {...rest}
+    />
+  );
+}
+
+const selectors = {
+  component: '.tyk-multiselect',
+  panel: '.tyk-multiselect__panel',
+  item: '.tyk-multiselect__list li',
+  itemControlsAddRemove: '.tyk-multiselect-item__controls button:nth-child(1)',
+  itemControlsDetails: '.tyk-multiselect-item__controls button:nth-child(2)',
+  itemDetails: '.tyk-multiselect-item__details',
+  message: '.tyk-message',
+  searchInput: 'input[name="search"]',
+};
+
 describe('Multiselect', () => {
-  it('TODO', () => {
-    expect(true).to.equal(true);
+  it('renders the component which has two panels', () => {
+    cy.mount(<Component />);
+
+    cy.get(selectors.component)
+      .should('exist')
+      .get(selectors.panel)
+      .should('have.length', 2);
+  });
+
+  it('calls the onChange callback with the selected item when clicking on the "add" control', () => {
+    const onChange = cy.stub().as('onChange');
+    const itemIndex = 2;
+    const item = defaultItems[itemIndex];
+    cy.mount(<Component onChange={onChange} />);
+
+    cy.get(selectors.item)
+      .eq(itemIndex)
+      .find(selectors.itemControlsAddRemove)
+      .click();
+
+    cy.get('@onChange')
+      .should('be.calledWith', [item]);
+  });
+
+  it('calls the getItemDetails callback when clicking on the "show details" control', () => {
+    const getItemDetails = cy.stub().as('getItemDetails');
+    const itemIndex = 3;
+    const item = defaultItems[itemIndex];
+    cy.mount(<Component getItemDetails={getItemDetails} />);
+
+    cy.get(selectors.item)
+      .eq(itemIndex)
+      .find(selectors.itemControlsDetails)
+      .click();
+
+    cy.get('@getItemDetails')
+      .should('be.calledWith', item);
+  });
+
+  it('the selected items panel shows a message if no item is selected and a list of the selected items otherwise', () => {
+    cy.mount(<Component />);
+
+    cy.get(selectors.panel)
+      .eq(0)
+      .as('selectedItemsPanel')
+      .find(selectors.message)
+      .should('exist');
+
+    cy.get(selectors.item)
+      .eq(2)
+      .find(selectors.itemControlsAddRemove)
+      .click();
+
+    cy.get('@selectedItemsPanel')
+      .find(selectors.message)
+      .should('not.exist');
+    cy.get('@selectedItemsPanel')
+      .find(selectors.item)
+      .should('have.length', 1);
+  });
+
+  it('you can remove items from being selected by clicking on the "remove" control of the selected item', () => {
+    const value = defaultItems.slice(0, 2);
+    cy.mount(<Component value={value} />);
+
+    cy.get(selectors.panel)
+      .eq(0)
+      .as('selectedItemsPanel');
+
+    cy.get('@selectedItemsPanel')
+      .find(selectors.item)
+      .should('have.length', value.length);
+
+    cy.get('@selectedItemsPanel')
+      .find(selectors.item)
+      .eq(0)
+      .find(selectors.itemControlsAddRemove)
+      .click();
+
+    cy.get('@selectedItemsPanel')
+      .find(selectors.item)
+      .should('have.length', value.length - 1);
+  });
+
+  it('you can filter items using the search input from a panel', () => {
+    cy.mount(<Component />);
+
+    cy.get(selectors.panel)
+      .eq(1)
+      .as('itemsPanel');
+
+    cy.get('@itemsPanel')
+      .find(selectors.item)
+      .should('have.length', defaultItems.length);
+
+    cy.get('@itemsPanel')
+      .find(selectors.searchInput)
+      .type('item 3');
+
+    cy.get('@itemsPanel')
+      .find(selectors.item)
+      .should('have.length', 1);
   });
 });
--- src/form/components/Multiselect/js/MultiselectPanel.js
+++ src/form/components/Multiselect/js/MultiselectPanel.js
@@ -1,6 +1,5 @@
-import React, { Component, createRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { fromJS } from 'immutable';
 
 import Input from '../../Input';
 import Icon from '../../../../components/Icon';
@@ -8,101 +7,73 @@ import Message from '../../../../components/Message';
 import InfiniteScroller from '../../../../components/InfiniteScroller';
 import MultiselectItem from './MultiselectItem';
 
-class MultiselectPanel extends Component {
-  static propTypes = {
-    customSearch: PropTypes.bool,
-    onCustomSearch: PropTypes.func,
-    panelType: PropTypes.string,
-    onChange: PropTypes.func,
-    items: PropTypes.instanceOf(Array),
-    itemsPageNumber: PropTypes.number,
-    itemsNrPages: PropTypes.number,
-    inputPlaceholder: PropTypes.string,
-    loadMoreItems: PropTypes.func,
-    noItemsMessage: PropTypes.string,
-    serverLoad: PropTypes.bool,
-  };
+function MultiselectPanel({
+  inputPlaceholder,
+  noItemsMessage,
+  items,
+  onCustomSearch,
+  onChange,
+  customSearch,
+  panelType,
+  serverLoad,
+  itemsPageNumber,
+  itemsNrPages,
+  loadMoreItems,
+}) {
+  const [searchValue, setSearchValue] = useState('');
+  const itemIndex = useRef(0);
+  const itemsListRef = useRef();
 
-  state = {
-    searchValue: '',
-  };
+  function getItemIndex() {
+    itemIndex.current += 1;
 
-  constructor(props) {
-    super(props);
-
-    this.searchOnChange = this.searchOnChange.bind(this);
-    this.itemChanged = this.itemChanged.bind(this);
-    this.itemIndex = 0;
-    this.itemsListRef = createRef();
+    return itemIndex.current;
   }
 
-  getItemIndex() {
-    this.itemIndex += 1;
-
-    return this.itemIndex;
-  }
-
-  getItemsList() {
-    const {
-      itemsPageNumber,
-      itemsNrPages,
-      loadMoreItems,
-      serverLoad,
-    } = this.props;
-
+  function getItemsList() {
     return (
       serverLoad
         ? (
           <InfiniteScroller
-            refChild={this.itemsListRef}
+            refChild={itemsListRef}
             hasMore={itemsNrPages > itemsPageNumber}
             loadMore={loadMoreItems}
             pageNumber={itemsPageNumber}
           >
-            { this.renderListItems() }
+            { renderListItems() }
           </InfiniteScroller>
         )
-        : this.renderListItems()
+        : renderListItems()
     );
   }
 
-  searchOnChange(value) {
-    const { onCustomSearch } = this.props;
-
-    if (!onCustomSearch) {
-      this.setState({
-        searchValue: value,
-      });
-    } else {
+  const searchOnChange = useCallback((value) => {
+    if (onCustomSearch) {
       onCustomSearch(value);
+    } else {
+      setSearchValue(value);
     }
-  }
+  }, [onCustomSearch]);
 
-  itemChanged(item) {
-    const { items, onChange } = this.props;
-
-    let cloneItems = fromJS(items).toJS();
-    cloneItems = cloneItems.filter(clonedItem => item.id !== clonedItem.id);
+  const itemChanged = useCallback((item) => {
+    let cloneItems = structuredClone(items);
+    cloneItems = cloneItems.filter((clonedItem) => item.id !== clonedItem.id);
 
     onChange(cloneItems, item);
-  }
+  }, [items]);
 
-  renderListItems() {
-    const {
-      customSearch, items, panelType, serverLoad,
-    } = this.props;
-    const { searchValue } = this.state;
+  function renderListItems() {
     return (
       <ul
         className={`tyk-multiselect__list${(!serverLoad) ? ' tyk-multiselect__list--scroll' : ''}`}
-        ref={this.itemsListRef}
+        ref={itemsListRef}
       >
         {
-          items.map(item => (
+          items.map((item) => (
             <MultiselectItem
               itemType={panelType}
-              key={this.getItemIndex()}
-              onChange={this.itemChanged}
+              key={getItemIndex()}
+              onChange={itemChanged}
               searchValue={customSearch ? '' : searchValue}
               item={item}
             />
@@ -112,39 +83,42 @@ class MultiselectPanel extends Component {
     );
   }
 
-  render() {
-    const {
-      inputPlaceholder,
-      noItemsMessage,
-      items,
-    } = this.props;
-    const { searchValue } = this.state;
-
-    return (
-      <div className="tyk-multiselect__panel">
-        <Input
-          inputgroupaddonleft={
-            <Icon type="search" />
-          }
-          name="search"
-          onChange={this.searchOnChange}
-          placeholder={inputPlaceholder}
-          value={searchValue}
-        />
-        {
-          items.length
-            ? this.getItemsList()
-            : (
-              <Message
-                theme="info"
-              >
-                { noItemsMessage }
-              </Message>
-            )
+  return (
+    <div className="tyk-multiselect__panel">
+      <Input
+        inputgroupaddonleft={
+          <Icon type="search" />
         }
-      </div>
-    );
-  }
+        name="search"
+        onChange={searchOnChange}
+        placeholder={inputPlaceholder}
+        value={searchValue}
+      />
+      {
+        items.length
+          ? getItemsList()
+          : (
+            <Message theme="info">
+              { noItemsMessage }
+            </Message>
+          )
+      }
+    </div>
+  );
 }
+
+MultiselectPanel.propTypes = {
+  customSearch: PropTypes.bool,
+  onCustomSearch: PropTypes.func,
+  panelType: PropTypes.string,
+  onChange: PropTypes.func,
+  items: PropTypes.instanceOf(Array),
+  itemsPageNumber: PropTypes.number,
+  itemsNrPages: PropTypes.number,
+  inputPlaceholder: PropTypes.string,
+  loadMoreItems: PropTypes.func,
+  noItemsMessage: PropTypes.string,
+  serverLoad: PropTypes.bool,
+};
 
 export default MultiselectPanel;
