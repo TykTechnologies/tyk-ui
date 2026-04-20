d0494b5088230ade8995d6cc1a8fa56f99717554
Vlad Zabolotnyi <109525963+vladzabolotnyi@users.noreply.github.com>
2026-04-20 08:32:43 +0300

    TT-14336: Update text ellipsis with functionality to truncate values in front and in the end (#416)
    * feat: add truncateFrom property to support dots and truncation logic
    
    * build: add alpha version
    
    * feat: add className property to text ellipsis
    
    * feat: add style and overlayClassName property to ellipsis to make it possible to pass z-index to floating container
    
    * build: changes after prod build run and version grade without alpha suffix
    
    ---------
    
    Co-authored-by: Vlad Zabolotnyi <vlad.z@tyk.io>

--- src/components/TextEllipsis/index.js
+++ src/components/TextEllipsis/index.js
@@ -1,32 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Tooltip from '../Tooltip';
+import React from "react";
+import PropTypes from "prop-types";
+import Tooltip from "../Tooltip";
 /**
  * TextEllipsis component helps you to hide a part of a text,
  * but displaying it when it's being hovered.
  * The entire text is displayed with the help of Tooltip component
  */
 
-function TextEllipsis({ text, limit, position }) {
+function TextEllipsis({
+  style,
+  className,
+  overlayClassName,
+  text,
+  limit,
+  position,
+  truncateFrom = "end",
+}) {
+  if (text.length <= limit) {
+    return text;
+  }
+
+  let displayText;
+
+  if (truncateFrom === "start") {
+    displayText = `...${text.substring(text.length - limit)}`;
+  } else {
+    displayText = `${text.substring(0, limit)}...`;
+  }
+
   return (
-    text.length > limit
-      ? (
-        <Tooltip render={text} position={position}>
-          {text.substring(0, limit)}
-          ...
-        </Tooltip>
-      )
-      : text
+    <Tooltip
+      style={style}
+      overlayClassName={overlayClassName}
+      className={className}
+      render={text}
+      position={position}
+    >
+      {displayText}
+    </Tooltip>
   );
 }
 
 TextEllipsis.propTypes = {
+  style: PropTypes.instanceOf(Object),
+  className: PropTypes.string,
+  overlayClassName: PropTypes.string,
   /** Text to be shrinked by TextEllipsis */
   text: PropTypes.string,
   /** Number of characters that TextEllipsis would leave visible */
   limit: PropTypes.number,
   /** tooltip position */
   position: PropTypes.string,
+  /** Where to truncate text from - 'end' (default) shows beginning of text, 'start' shows end of text */
+  truncateFrom: PropTypes.oneOf(["end", "start"]),
 };
 
 export default TextEllipsis;
